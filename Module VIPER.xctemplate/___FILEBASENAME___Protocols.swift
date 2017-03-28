65f639e6351293fe6ca56595e497c948e2b8881b
Brandon Leeds <brandonleeds@gmail.com>
2017-03-28 13:17:30 -0400

    Fix formatting

--- Module VIPER.xctemplate/___FILEBASENAME___Protocols.swift
+++ Module VIPER.xctemplate/___FILEBASENAME___Protocols.swift
@@ -8,23 +8,23 @@
 
 import Foundation
 
-//MARK: Wireframe -
+// MARK: Wireframe -
 protocol ___FILEBASENAMEASIDENTIFIER___WireframeProtocol: class {
 
 }
-//MARK: Presenter -
+// MARK: Presenter -
 protocol ___FILEBASENAMEASIDENTIFIER___PresenterProtocol: class {
 
 }
 
-//MARK: Interactor -
+// MARK: Interactor -
 protocol ___FILEBASENAMEASIDENTIFIER___InteractorProtocol: class {
 
-  var presenter: ___FILEBASENAMEASIDENTIFIER___PresenterProtocol?  { get set }
+  var presenter: ___FILEBASENAMEASIDENTIFIER___PresenterProtocol? { get set }
 }
 
-//MARK: View -
+// MARK: View -
 protocol ___FILEBASENAMEASIDENTIFIER___ViewProtocol: class {
 
-  var presenter: ___FILEBASENAMEASIDENTIFIER___PresenterProtocol?  { get set }
+  var presenter: ___FILEBASENAMEASIDENTIFIER___PresenterProtocol? { get set }
 }
